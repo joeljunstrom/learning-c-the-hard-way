26e62348baa42c887d17e525dc00ccc0a40c1b47
Joel Junström <joel.junstrom@oktavilla.se>
2012-05-04 09:08:33 +0200

    Using strlen

--- ex14.c
+++ ex14.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // forward declarations
-void print_letters(char arg[]);
+void print_letters(char arg[], int arg_length);
 
 void print_arguments(int argc, char *argv[])
 {
 	int i = 0;
 
 	for(i = 0; i < argc; i++) {
-		print_letters(argv[i]);
+		char *arg = argv[i];
+		int arg_length = strlen(arg);
+
+		print_letters(arg, arg_length);
 	}
 }
 
-void print_letters(char arg[])
+void print_letters(char arg[], int arg_length)
 {
 	int i = 0;
 
-	for (i = 0; arg[i] != '\0'; i++) {
+	for (i = 0; i <= arg_length; i++) {
 		char ch = arg[i];
 
 		if(isalpha(ch) || isblank(ch)) {
